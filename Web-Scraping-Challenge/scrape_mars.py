7959ee78175318acbdb282642c9f42fbabde019a
Vince Bober <Vince@Mattie.attlocal.net>
2019-11-03 21:16:29 -0500

    update

--- Web-Scraping-Challenge/scrape_mars.py
+++ Web-Scraping-Challenge/scrape_mars.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 import time
 
-
-weather_url = "https://twitter.com/marswxreport?lang=en"
 facts_url = "https://space-facts.com/mars/"
 hemishpere_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
@@ -24,22 +22,62 @@ def scrape_info():
     title = soup.find_all("div", class_="content_title")[0].text
     headline_text = soup.find_all("div", class_="article_teaser_body")[0].text
 
-    
-
     feature_img_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    # browswer = init_browser()
+    feature_img_url2 = "https://www.jpl.nasa.gov"
     browser.get(feature_img_url)
     time.sleep(1)
     html2 = browser.page_source
     soup2 = bs(html2, "lxml")
 
-    style = soup2.find()"article",class_="carousel_item")[0]
-    
-    image = style.split("('", 1)[1].split("')")[0]
+    image = soup2.find("article", class_="carousel_item")['style']
+    feature_image = image.split("('", 1)[1].split("')")[0]
 
-    feature_img = feature_img_url + image
+    feature_img = feature_img_url2 + feature_image
 
-    data = {"title": title, "headline_text": headline_text,"feature_img":feature_img}
+    weather_url = "https://twitter.com/marswxreport?lang=en"
+    browser.get(weather_url)
+    time.sleep(1)
+    html3 = browser.page_source
+    soup3 = bs(html3, "lxml")
+    weather = soup3.find("p", class_="TweetTextSize").text
+
+    hemishpere_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.get(hemishpere_url)
+    html4 = browser.page_source
+    soup4 = bs(html4, "lxml")
+    time.sleep(1)
+    hemisphere_img_urls = {}
+    all_titles = soup4.find_all('h3')
+    y = len(all_titles)
+    for x in range(0, y):
+        hemisphere_img_urls["title"] = all_titles[x].text
+        doc = browser.find_element_by_link_text(all_titles[x].text)
+        doc.click()
+        time.sleep(1)
+        html4 = browser.page_source
+        soup4 = bs(html4, "lxml")
+        hemisphere_img_urls["img_url"] = soup4.find_all('a', target='_blank')[x]['href']
+        browser.back()
+    print(hemisphere_img_urls)
+
+
+# hemisphere_image_urls = [
+#     {"title": "Valles Marineris Hemisphere", "img_url": "..."},
+#     {"title": "Cerberus Hemisphere", "img_url": "..."},
+#     {"title": "Schiaparelli Hemisphere", "img_url": "..."},
+#     {"title": "Syrtis Major Hemisphere", "img_url": "..."},
+# ]
+
+# {% for hemisphere in mars_data.hemisphere %}
+#     <div>
+#       <img src="{{hemisphere.url}}">
+#       <h3>{{hemisphere.title}}</h3>
+#     </div>
+#     {% endfor %}
+
+    browser.quit()
+
+    data = {"title": title, "headline_text": headline_text,
+            "feature_img": feature_img, "weather": weather}
     browser.close()
     return data
-/html/body/div[1]/div/div[3]/section[1]/div/div/article
