9b015ee92247deacd3b08bdf916bec20f27846c1
Vince Bober <Vince@Mattie.attlocal.net>
2019-11-10 21:31:00 -0500

    update

--- Javascript-Challenge/Instructions/StarterCode/static/js/app.js
+++ Javascript-Challenge/Instructions/StarterCode/static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 var tableData = data;
 
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 button.on("click", function() {
     var inputElement = d3.select("#datetime");
@@ -9,6 +9,26 @@ button.on("click", function() {
     console.log(inputValue);
     console.log(tableData);
 
-    var filterData = tableData.filter(sighting => sighting.datetime ===inputValue);
-    console.log(filterData);
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(filteredData);
+
+    d3.select(".table-striped").selectAll("tbody")
+        .selectAll("tr")
+        .data(filteredData)
+        .enter()
+        .append("tr")
+        .html(function(d) {
+            return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`
+    });
+
 });
+
+
+// d3.select(".table-striped").selectAll("tbody").selectAll("tr")
+//   .data(austinWeather)
+//   .enter()
+//   .append("tr")
+//   .html(function(d) {
+//     return `<td>${d.date}</td><td>${d.low}</td><td>${d.high}</td>`;
+//   })
